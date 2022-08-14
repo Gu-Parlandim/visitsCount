05f51de435dc2af4e7a0e30b38ec28e549e78701
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-08-14 17:25:48 -0300

    removendo temas não usados

--- db/mongodb.js
+++ db/mongodb.js
@@ -2,6 +2,7 @@
 
 const mongoose = require("mongoose");
 const schema = require("./schema");
+require("dotenv").config();
 
 // the default mongodb url (local server)
 const mongodbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/count";
@@ -9,7 +10,6 @@ const mongodbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/count";
 mongoose.connect(mongodbURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
 });
 
 const Count = mongoose.connection.model("Count", schema);
