0f1c834e59e5757884b7c491870592723f058b2d
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-08-15 12:48:12 -0300

    adicionando header content-type

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
   res.render("index");
 });
 
