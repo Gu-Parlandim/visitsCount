3feef0753ba03872f4b3e5732bdc5cacefa4ab9b
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-08-15 13:11:07 -0300

    test

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/html", " text/css");
   res.render("index");
 });
 
